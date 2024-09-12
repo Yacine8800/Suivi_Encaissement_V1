e7d9d81b60a9eaeb2c7681c9f1b4e2ae4bb271cd
Yacine8800 <71154342+Yacine8800@users.noreply.github.com>
2024-09-12 09:03:16 +0000

    fix:login

--- components/auth/login/loginUser.tsx
+++ components/auth/login/loginUser.tsx
@@ -6,146 +6,149 @@ import Image from "next/image";
 import ForgotPasswordModal from "../components/modals/ForgotPasswordModal";
 
 const ComponentsAuthLoginForm = () => {
-    const router = useRouter();
-    const [showPassword, setShowPassword] = useState(false);
-    const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false); // State for the modal
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); // State for the modal
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
     };
 
-    React.useEffect(() => {
-        const handleResize = () => {
-            setIsSmallScreen(window.innerWidth <= 768);
-        };
+    handleResize(); // Initial call to set the state correctly
+    window.addEventListener("resize", handleResize);
 
-        handleResize(); // Initial call to set the state correctly
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    const submitForm = (e: any) => {
-        e.preventDefault();
-        router.push("/dashboard");
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    router.push("/dashboard");
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    return (
-        <div className="flex h-screen items-center justify-center relative overflow-hidden">
-            {/* Dividing the page into two sections */}
-            <div className="absolute inset-0 flex">
-                <div className="w-1/2 bg-[#FFDFBD] flex items-center justify-center relative">
-                    {/* Left animated SVGs or images */}
-                </div>
-                <div className="w-1/2 bg-[#F07D00] flex items-center justify-center relative">
-                    {/* Right animated SVGs or images */}
-                </div>
-            </div>
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-            {/* Logo at the top left */}
-            <div className="absolute top-5 left-5 z-10">
-                <Image
-                    width={181}
-                    height={82}
-                    src="/assets/images/auth/logo.png" // Path to the logo
-                    alt="Logo"
-                    className="h-12 w-auto"
-                />
-            </div>
-
-            {/* Login box with conditional padding */}
-            <div
-                className={`relative z-20 w-full max-w-lg bg-white rounded-[30px] shadow-xl p-8 ${isSmallScreen ? "mx-4" : ""
-                    }`} // Adding 'mx-4' padding on small screens
-            >
-                {/* Title and logo in the form */}
-                <div className="flex justify-between items-center">
-                    <h2 className="text-3xl font-bold text-gray-900">Login</h2>
-                    <Image
-                        width={100}
-                        height={100}
-                        src={"/assets/images/logo2.png"}
-                        alt=""
-                    />
-                </div>
-                <p className="text-gray-500 font-bold">Bienvenue à Suivi Encaissement !</p>
-
-                <form className="space-y-6 pt-5" onSubmit={submitForm}>
-                    <div>
-                        <label
-                            htmlFor="Email"
-                            className="block text-sm font-medium text-gray-700"
-                        >
-                            Entrez votre adresse mail
-                        </label>
-                        <div className="relative mt-1">
-                            <input
-                                id="Email"
-                                type="email"
-                                placeholder="johndoe@gmail.com"
-                                className="w-full py-3 px-4 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label
-                            htmlFor="Password"
-                            className="block text-sm font-medium text-gray-700"
-                        >
-                            Entrez votre mot de passe
-                        </label>
-                        <div className="relative mt-1">
-                            <input
-                                id="Password"
-                                type={showPassword ? "text" : "password"}
-                                placeholder="Mot de passe"
-                                className="w-full py-3 px-4 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                                required
-                            />
-                            <span
-                                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                                onClick={togglePasswordVisibility}
-                            >
-                                <IconEye
-                                    className={showPassword ? "text-orange-500" : "text-gray-500"}
-                                />
-                            </span>
-                        </div>
-                    </div>
-                    <div className="flex justify-end">
-                        <button
-                            type="button"
-                            className="text-sm text-gray-500 hover:text-orange-500"
-                            onClick={openModal} // Open the modal when clicked
-                        >
-                            Mot de passe oublié?
-                        </button>
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full py-3 px-4 text-white bg-orange-500 hover:bg-orange-600 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                    >
-                        Sign in
-                    </button>
-                </form>
-            </div>
-
-            {/* Modal */}
-            {isModalOpen && <ForgotPasswordModal closeModal={closeModal} />}
+  return (
+    <div className="relative flex h-screen items-center justify-center overflow-hidden">
+      {/* Dividing the page into two sections */}
+      <div className="absolute inset-0 flex">
+        <div className="relative flex w-1/2 items-center justify-center bg-[#FFDFBD]">
+          {/* Left animated SVGs or images */}
         </div>
-    );
+        <div className="relative flex w-1/2 items-center justify-center bg-[#F07D00]">
+          {/* Right animated SVGs or images */}
+        </div>
+      </div>
+
+      {/* Logo at the top left */}
+      <div className="absolute left-5 top-5 z-10">
+        <Image
+          width={181}
+          height={82}
+          src="/assets/images/auth/logo.png" // Path to the logo
+          alt="Logo"
+          className="h-12 w-auto"
+        />
+      </div>
+
+      {/* Login box with conditional padding */}
+      <div
+        className={`relative z-20 w-full max-w-lg rounded-[30px] bg-white p-8 shadow-xl ${
+          isSmallScreen ? "mx-4" : ""
+        }`} // Adding 'mx-4' padding on small screens
+      >
+        {/* Title and logo in the form */}
+        <div className="flex items-center justify-between">
+          <h2 className="text-3xl font-bold text-gray-900">Login</h2>
+          <Image
+            width={100}
+            height={100}
+            src={"/assets/images/logo2.png"}
+            alt=""
+          />
+        </div>
+        <p className="font-bold text-gray-500">
+          Bienvenue à Suivi Encaissement !
+        </p>
+
+        <form className="space-y-6 pt-5" onSubmit={submitForm}>
+          <div>
+            <label
+              htmlFor="Email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Entrez votre adresse mail
+            </label>
+            <div className="relative mt-1">
+              <input
+                id="Email"
+                type="email"
+                placeholder="johndoe@gmail.com"
+                className="w-full rounded-md border border-gray-300 px-4 py-3 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="Password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Entrez votre mot de passe
+            </label>
+            <div className="relative mt-1">
+              <input
+                id="Password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Mot de passe"
+                className="w-full rounded-md border border-gray-300 px-4 py-3 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                required
+              />
+              <span
+                className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <IconEye
+                  className={showPassword ? "text-orange-500" : "text-gray-500"}
+                />
+              </span>
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="text-sm text-gray-500 hover:text-orange-500"
+              onClick={openModal} // Open the modal when clicked
+            >
+              Mot de passe oublié?
+            </button>
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-md bg-orange-500 px-4 py-3 text-white shadow-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            Connexion
+          </button>
+        </form>
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && <ForgotPasswordModal closeModal={closeModal} />}
+    </div>
+  );
 };
 
 export default ComponentsAuthLoginForm;
