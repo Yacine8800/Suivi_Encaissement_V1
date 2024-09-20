6909336365b859fa79a6e39f8ed2548bd481bac5
venancekpro <venancek.pro@gmail.com>
2024-09-20 14:21:35 +0000

    ajout du boutton voir plus et de la popup (Section litiges)

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 export const API_URL = process.env.REACT_API_EMS_URL as string;
-export const API_FCMS = process.env.REACT_API_FCMS_URL as string;
+export const API_FCMS = "https://fraudprod.smartcontrol-cie.com/api/v1";
 export const API_FCMS_PHOTO = process.env.REACT_API_FCMS_PHOTO_URL as string;
 export const API_AMS = process.env.REACT_API_AMS_URL as string;
 export const API_GATEWAY_EMS = process.env.REACT_API_GATEWAY_URL as string;
--- store/reducers/users-test/user.slice.ts
+++ store/reducers/users-test/user.slice.ts
@@ -4,11 +4,42 @@ import { createSelector, createSlice } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { persistStore } from "redux-persist";
 import { initialStateMapCie } from "./user.initial";
+import { fetchUsersList } from "./user.async";
 
 const userSlice = createSlice({
 	name: "userList",
 	initialState: initialStateMapCie,
 	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchUsersList.pending, (state) => {
+				state.statutUser = "loading";
+				//console.log("Loading Agence Cie",ALoading++);
+				// showLoadingToast("Chargement des agences..."); // Afficher le message de chargement
+			})
+			.addCase(fetchUsersList.fulfilled, (state, action) => {
+				state.statutUser = "succeeded";
+				state.userData = action.payload;
+				// hideLoadingToast(); // Masquer le message de chargement
+				//console.log("response2.data",state.statusSuspectsList);
+			})
+			.addCase(fetchUsersList.rejected, (state, action) => {
+				state.statutUser = "failed";
+				state.errorUser = action.error.message || null;
+				// hideLoadingToast(); // Masquer le message de chargement
+				//console.log("Loading rejected Agence Cie", state.statusSuspectsList);
+				// Afficher un message d'erreur
+				// toast.error("Erreur lors du chargement des agences.", {
+				// 	position: "top-right",
+				// 	autoClose: 3000,
+				// 	hideProgressBar: false,
+				// 	closeOnClick: true,
+				// 	pauseOnHover: true,
+				// 	draggable: true,
+				// 	progress: undefined,
+				// });
+			});
+	},
 });
 
 // export const { setUser } = userSlice.actions;
@@ -17,9 +48,9 @@ export default userSlice.reducer;
 // Ici je crée mes sélecteurs pour chaque données que je veux
 // Pour nous empechers de réecrire TRootState a chaque fois que l'on à besoin des données
 // On l'initialise une fois ici
-export const usersDataSelector = (state: TRootState) => state.user;
+export const usersDataSelector = (state: TRootState) => state.user.userData;
 
-export const selectAgenceList = createSelector(
+export const UserData = createSelector(
 	[usersDataSelector],
 	(userList) => userList
 );
